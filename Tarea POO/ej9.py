da19cdde3482aa0cbecd7bf0639b3e376c1ebaf1
David Carrillo <113156868+SerpentingK@users.noreply.github.com>
2024-06-27 11:35:20 -0500

    Delete Tarea POO directory

--- Tarea POO/ej9.py
+++
@@ -1,8 +0,0 @@
-from clases import Paquete
-
-
-paquete_1 = Paquete("123456789", "Ciudad A", "Ciudad B")
-print(paquete_1.consultar_ubicacion())
-
-paquete_1.actualizar_estado("En almacén")
-print(paquete_1.consultar_ubicacion())
